dabc3e3d0c946a869022d997c8176817d8ca3133
Arthur.Khongwh <Arthur.KhongWH@finpoints.com>
2023-02-24 17:14:34 +0800

    added job list and included PropType  to use typescript on props

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="app">
-    <p>{{ jobs[0].location }}</p>
-    
+    <JobList :jobs="jobs"></JobList>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref} from "vue";
+import { defineComponent, ref } from "vue";
+import JobList from "./components/JobList.vue";
 //we can import a custom type to use
-import Job from './types/job'
+import Job from "./types/job";
 
 // this is required for TS where we specify defineComponent
 export default defineComponent({
   name: "App",
+  components: {
+    JobList,
+  },
   setup() {
     //using reactive
     // const state = reactive({
@@ -27,18 +30,36 @@ export default defineComponent({
 
     // if we want to use TS using ref we need to include <> and define the type after ref as ref is an object
     const jobs = ref<Job[]>([
-      { title: 'farm worker', location: 'lon lon ranch', salary: 30000, id: '1' },
-      { title: 'quarryman', location: 'death mountain', salary: 40000, id: '2' },
-      { title: 'flute player', location: 'the lost woods', salary: 35000, id: '3' },
-      { title: 'fisherman', location: 'lake hylia', salary: 21000, id: '4' },
-      { title: 'prison guard', location: 'gerudo valley', salary: 32000, id: '5' }
-    ])
+      {
+        title: "farm worker",
+        location: "lon lon ranch",
+        salary: 30000,
+        id: "1",
+      },
+      {
+        title: "quarryman",
+        location: "death mountain",
+        salary: 40000,
+        id: "2",
+      },
+      {
+        title: "flute player",
+        location: "the lost woods",
+        salary: 35000,
+        id: "3",
+      },
+      { title: "fisherman", location: "lake hylia", salary: 21000, id: "4" },
+      {
+        title: "prison guard",
+        location: "gerudo valley",
+        salary: 32000,
+        id: "5",
+      },
+    ]);
 
-    return {jobs}
+    return { jobs };
   },
-  methods: {
-    
-  },
+  methods: {},
 });
 </script>
 
