31e07daa8e468798f5935d56fb31171f443daed3
Arthur.Khongwh <Arthur.KhongWH@finpoints.com>
2023-02-24 16:32:27 +0800

    using custom types with composition API

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,45 @@
 <template>
- <div class="app">
-    <p>
-      {{name}} - {{ age }}
-    </p>
-    <button @click="changeName('Zelda')" >Change Name</button>
-    <button @click="changeAge(30)" >Change Name</button>
- </div>
-  
+  <div class="app">
+    <p>{{ jobs[0].location }}</p>
+    
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-
+import { defineComponent, ref} from "vue";
+//we can import a custom type to use
+import Job from './types/job'
 
 // this is required for TS where we specify defineComponent
 export default defineComponent({
-  name: 'App',
+  name: "App",
+  setup() {
+    //using reactive
+    // const state = reactive({
+    //   name: "Link",
+    //   age: 25 as string | number,
+    // });
+    // state.name = 999
+    // state.age  = false
+    // return {
+    //   ...toRefs(state),
+    // };
 
-  data() {
-    return  {
-      name: 'Link',  //TS will know this must always be a string
-      // we use type assertion if we have multiple types the keyword being  as
-      age:  25 as number | string
-    }
+    // if we want to use TS using ref we need to include <> and define the type after ref as ref is an object
+    const jobs = ref<Job[]>([
+      { title: 'farm worker', location: 'lon lon ranch', salary: 30000, id: '1' },
+      { title: 'quarryman', location: 'death mountain', salary: 40000, id: '2' },
+      { title: 'flute player', location: 'the lost woods', salary: 35000, id: '3' },
+      { title: 'fisherman', location: 'lake hylia', salary: 21000, id: '4' },
+      { title: 'prison guard', location: 'gerudo valley', salary: 32000, id: '5' }
+    ])
+
+    return {jobs}
   },
-  methods:{
-    //we need to specify the type of the arg
-    changeName(name: string){
-      this.name  = name
-      return name
-    },
-    changeAge(age: string | number ){
-      this.age = age
-      return age
-    }
-  }
+  methods: {
+    
+  },
 });
 </script>
 
-<style>
-
-</style>
+<style></style>
